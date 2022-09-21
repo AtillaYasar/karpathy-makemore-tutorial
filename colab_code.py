df268b13441180fb5dd0db8362b886b6a9ec1954
AtillaYasar <112716905+AtillaYasar@users.noreply.github.com>
2022-09-21 14:11:03 +0200

    Update colab_code.py

--- colab_code.py
+++ colab_code.py
@@ -1,5 +1,3 @@
-# the dotted lines denote the beginning and end of a code block
-
 # ------------------------------------------------------------------
 
 # making a character-level 'language model' (multilayer perceptron), trained on a list of names.
@@ -80,9 +78,17 @@ def toJson(obj, filename, overwrite = 0):
 # setting hyperparameters, building datasets, training the model
 
 # model parameters
-context_size = 5
-embedding_dimension = 5
-hidden_dimension = 300
+context_size = 4
+embedding_dimension = 4
+hidden_dimension = 200
+
+# training parameters
+total_iterations = 30000
+batch_size = 200
+
+# put stuff in handy dictionaries
+training_params = {'total_iterations':total_iterations,
+                   'batch_size':batch_size}
 model_params = {'context_size':context_size,
                 'embedding_dimension':embedding_dimension,
                 'hidden_dimension':hidden_dimension}
@@ -90,15 +96,6 @@ model_params = {'context_size':context_size,
 # initialize weights
 C, W1, b1, W2, b2 = get_parameters(**model_params)
 parameters = [C, W1, b1, W2, b2]
-
-# training parameters
-total_iterations = 50000
-batch_size = 50
-lrs = [0.1, 0.01, 0.001]
-thresholds = list(map(lambda fl:int(round(fl, 0)), [((n+1)/len(lrs))*total_iterations for n in range(len(lrs))]))
-training_params = {'total_iterations':total_iterations,
-                   'batch_size':batch_size,
-                   'learning rates':f'lrs: {lrs}, thresholds: {thresholds}'}
 
 # building the training, dev and test datasets. 80/10/10 split.
 random.seed(42)
@@ -110,15 +107,8 @@ Xtr, Ytr = build_dataset(words[:n1], context_size)
 Xdev, Ydev = build_dataset(words[n1:n2], context_size)
 Xte, Yte = build_dataset(words[n2:], context_size)
 
-# janky way of adjusting the learning rate
-def get_lr(lrs, thresholds, i):
-  for n, t in enumerate(thresholds):
-    if i < t:
-      return lrs[n]
 
 # for tracking stats
-track_frequency = 1000
-track_every = total_iterations//track_frequency
 stats = {
          'model_params': model_params,
          'training_params':training_params,
@@ -127,11 +117,12 @@ stats = {
          'training time':0,
          'start at':str(time.time()).partition('.')[0],
          'end at':'',
-         'track_frequency':track_frequency,
          'losses':{}
          }
 
-track_counter = 0
+lr = 1
+lrs = []
+losses = []
 # start training
 for i in range(total_iterations):
 
@@ -144,25 +135,33 @@ for i in range(total_iterations):
   logits = h @ W2 + b2
   loss = F.cross_entropy(logits, Ytr[ix])
 
+  if 1:
+    losses.append(loss.log10().item())
+  else:
+    losses.append(loss.item())
+  lrs.append(lr)
+
   # backward pass
   for p in parameters:
     p.grad = None
   loss.backward()
+  
+  if i > 2000:
+    lr = 0.01
+  elif i > 1000:
+    lr = 0.1
+  else:
+    lr = 1
 
-  # update
-  lr = get_lr(lrs, thresholds, i)
+  # change parameters
   for p in parameters:
     p.data += -lr * p.grad
-  
-  # track stats
-  if track_counter > track_every:
-    stats['losses'][i] = {'lr':lr, 'loss':loss.item()}
-    track_counter = 0
-  else:
-    track_counter += 1
 
 stats['end at'] = str(time.time()).partition('.')[0]
+stats['losses'] = losses
 stats['training time'] = int(stats['end at']) - int(stats['start at'])
+
+print(f'cont:{context_size}, emb:{embedding_dimension}, hidden:{hidden_dimension}, batch:{batch_size}, iterations:{total_iterations}, time:{stats["training time"]}')
 
 # eval training loss
 emb = C[Xtr]
@@ -170,6 +169,8 @@ h = torch.tanh(emb.view(-1, model_params['context_size']*model_params['embedding
 logits = h @ W2 + b2
 loss = F.cross_entropy(logits, Ytr)
 stats['training loss'] = loss.item()
+
+print(f'training loss: {stats["training loss"]}')
 
 # eval dev loss
 emb = C[Xdev]
@@ -179,13 +180,188 @@ loss = F.cross_entropy(logits, Ydev)
 stats['dev loss'] = loss.item()
 
 print(f'dev loss: {stats["dev loss"]}')
+
+
+if 0:
+  print(f'dev loss: {stats["dev loss"]}')
+  print(f'stats: {stats}')
+  print(len(losses))
 toJson(stats, str(time.time()).partition('.')[0], overwrite=0)
 # ------------------------------------------------------------------
 
 # ------------------------------------------------------------------
 # plotting stuff
-steps = list(stats['losses'].keys())
-losses = [d['loss'] for d in stats['losses'].values()]
-lrs_used = [d['lr'] for d in stats['losses'].values()]
-plt.plot(steps, losses)
+
+if 0:
+  plt.plot(range(len(losses)), lrs)
+else:
+  plt.plot(range(len(losses)), losses)
+# ------------------------------------------------------------------
+
+# ------------------------------------------------------------------
+
+# a rewrite of the previous code but with 2 extra hidden layers.
+
+def get_bigboi_parameters(context_size, embedding_dimension, hidden_dimension):
+  g = torch.Generator().manual_seed(2147483647)
+  input_dimension = 27
+  output_dimension = 27
+  
+  # weights connecting the one_hot encoded inputs to the embedding layer
+  # though the one-hots are used to index into C, not multiplied by C.
+  C = torch.randn((input_dimension, embedding_dimension), generator=g)
+
+  # the weights connecting the embedding layer to the hidden layer
+  W1 = torch.randn((context_size*embedding_dimension, hidden_dimension), generator=g)
+  b1 = torch.randn(hidden_dimension, generator=g)
+
+  # the weights connecting the hidden layers to the output layers (logits)
+  W2 = torch.randn((int(hidden_dimension), int(hidden_dimension)), generator=g)
+  b2 = torch.randn(W2.shape[1], generator=g)
+  
+  W3 = torch.randn((int(hidden_dimension), int(hidden_dimension)), generator=g)
+  b3 = torch.randn(W3.shape[1], generator=g)
+  
+  W4 = torch.randn((int(hidden_dimension), output_dimension), generator=g)
+  b4 = torch.randn(W4.shape[1], generator=g)
+
+  # i forgot why i do this.
+  parameters = [C, W1, b1, W2, b2, W3, b3, W4, b4]
+  for p in parameters:
+    p.requires_grad = True
+
+  ridiculous = {s:'-'.join(map(str,tup)) for s, tup in list(zip('C,W1,b1,W2,b2,W3,b3,W4,b4'.split(','), [ [p.shape[i] for i in range(len(p.shape))] for p in parameters]))}
+  print(ridiculous)
+  return C, W1, b1, W2, b2, W3, b3, W4, b4
+# ------------------------------------------------------------------
+
+# ------------------------------------------------------------------
+
+# setting hyperparameters, building datasets, training the model
+
+# model parameters
+context_size = 3
+embedding_dimension = 3
+hidden_dimension = 100
+
+# training parameters
+total_iterations = 20000
+batch_size = 50
+gravity = 0
+
+# put stuff in handy dictionaries
+training_params = {'total_iterations':total_iterations,
+                   'batch_size':batch_size}
+model_params = {'context_size':context_size,
+                'embedding_dimension':embedding_dimension,
+                'hidden_dimension':hidden_dimension}
+
+# initialize weights
+C, W1, b1, W2, b2, W3, b3, W4, b4 = get_bigboi_parameters(**model_params)
+parameters = [C, W1, b1, W2, b2, W3, b3, W4, b4]
+
+
+# building the training, dev and test datasets. 80/10/10 split.
+random.seed(42)
+random.shuffle(words)
+n1 = int(0.8*len(words))
+n2 = int(0.9*len(words))
+# create datasets
+Xtr, Ytr = build_dataset(words[:n1], context_size)
+Xdev, Ydev = build_dataset(words[n1:n2], context_size)
+Xte, Yte = build_dataset(words[n2:], context_size)
+
+
+# for tracking stats
+stats = {
+         'model_params': model_params,
+         'training_params':training_params,
+         'training loss':0,
+         'dev loss':0,
+         'training time':0,
+         'start at':str(time.time()).partition('.')[0],
+         'end at':'',
+         'losses':{}
+         }
+
+lr = 1
+lrs = []
+losses = []
+# start training
+for i in range(total_iterations):
+
+  # construct minibatch
+  ix = torch.randint(0, Xtr.shape[0], (batch_size,))
+
+  # forward pass
+  emb = C[Xtr[ix]]
+  h1 = torch.tanh(emb.view(-1, model_params['context_size']*model_params['embedding_dimension']) @ W1 + b1)
+  h2 = torch.tanh(h1 @ W2 + b2)
+  h3 = torch.tanh(h2 @ W3 + b3)
+  logits = h3 @ W4 + b4
+  loss = F.cross_entropy(logits, Ytr[ix])# + gravity*( (W1**2).mean() + (W2**2).mean() + (W3**2).mean() + (W4**2).mean() )
+
+  if 1:
+    losses.append(loss.log10().item())
+  else:
+    losses.append(loss.item())
+  lrs.append(lr)
+
+  # backward pass
+  for p in parameters:
+    p.grad = None
+  loss.backward()
+  
+  if i > 1000:
+    lr = 0.01
+  else:
+    lr = 0.1
+
+  # change parameters
+  for p in parameters:
+    p.data += -lr * p.grad
+
+stats['end at'] = str(time.time()).partition('.')[0]
+stats['losses'] = losses
+stats['training time'] = int(stats['end at']) - int(stats['start at'])
+
+print(f'cont:{context_size}, emb:{embedding_dimension}, hidden:{hidden_dimension}, batch:{batch_size}, iterations:{total_iterations}, time:{stats["training time"]}')
+
+# eval training loss
+emb = C[Xtr]
+h1 = torch.tanh(emb.view(-1, model_params['context_size']*model_params['embedding_dimension']) @ W1 + b1)
+h2 = torch.tanh(h1 @ W2 + b2)
+h3 = torch.tanh(h2 @ W3 + b3)
+logits = h3 @ W4 + b4
+loss = F.cross_entropy(logits, Ytr)# + 0.01*(W1**2).mean() + gravity*( (W1**2).mean() + (W2**2).mean() + (W3**2).mean() + (W4**2).mean() )
+stats['training loss'] = loss.item()
+
+print(f'training loss: {stats["training loss"]}')
+
+# eval training loss
+emb = C[Xdev]
+h1 = torch.tanh(emb.view(-1, model_params['context_size']*model_params['embedding_dimension']) @ W1 + b1)
+h2 = torch.tanh(h1 @ W2 + b2)
+h3 = torch.tanh(h2 @ W3 + b3)
+logits = h3 @ W4 + b4
+loss = F.cross_entropy(logits, Ydev)# + 0.01*(W1**2).mean() + gravity*( (W1**2).mean() + (W2**2).mean() + (W3**2).mean() + (W4**2).mean() )
+stats['dev loss'] = loss.item()
+
+print(f'dev loss: {stats["dev loss"]}')
+
+
+if 0:
+  print(f'dev loss: {stats["dev loss"]}')
+  print(f'stats: {stats}')
+  print(len(losses))
+toJson(stats, str(time.time()).partition('.')[0], overwrite=0)
+# ------------------------------------------------------------------
+
+# ------------------------------------------------------------------
+# plotting stuff
+
+if 0:
+  plt.plot(range(len(losses)), lrs)
+else:
+  plt.plot(range(len(losses)), losses)
 # ------------------------------------------------------------------
